c65d33971ef38c3b0f7b41ca87e159f410aecdc7
solusfacere <jamesbjoy7@gmail.com>
2024-10-24 20:54:17 +0900

    lots of things

---
+++ importsys.py
@@ -0,0 +1,27 @@
+# import sys
+# import platform
+# print(f"Python version: {sys.version}")
+# print(f"Architecture: {platform.architecture()[0]}")
+
+import talib
+print(talib.__version__)  # Should print 0.4.28
+
+
+"""
+pip uninstall numpy
+pip uninstall TA-Lib
+pip install numpy --upgrade
+pip install TA_Lib-0.4.28-cp311-cp311-win_amd64.whl --upgrade
+"""
+
+"""
+python -m venv myenv
+myenv\Scripts\activate
+pip uninstall numpy
+pip uninstall TA-Lib
+pip install numpy
+pip install TA_Lib-0.4.28-cp311-cp311-win_amd64.whl
+"""
+
+# pip uninstall TA-Lib
+# pip install TA_Lib-0.4.28-cp311-cp311-win_amd64.whl  
